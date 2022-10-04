1f5354b7a0c22ad71997b74168aee79dfc70018f
Alex Erf <alexerf@icloud.com>
2022-10-03 22:39:29 -0500

    exercises up to 51

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,38 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut counter = 0;
+    let startIndex = loop {
+        if input.as_bytes()[counter] != b' ' || counter == input.len() {
+            break counter;
+        }
+        counter += 1;
+    };
+    counter = input.len() - 1;
+    let endIndex = loop {
+        if input.as_bytes()[counter] != b' ' || counter == 0 {
+            break counter;
+        }
+        counter -= 1;
+    };
+    input[startIndex..endIndex + 1].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let idx = input.find("cars");
+    match idx {
+        None => input.to_string(),
+        Some(i) => format!("{}balloons{}", &input.to_string()[0..i], &input.to_string()[i + 4..])
+    }
 }
 
 #[cfg(test)]
